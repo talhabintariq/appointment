b3f48f2d3c5369d5c5969cd727dcd5a7811ea99a
Talha <talhabintariq@gmail.com>
2019-08-10 22:47:04 -0400

    Initial Work

---
+++ src/components/header/header.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Button } from 'antd';
+
+export default class Header extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Button type="primary">Button</Button>
+      </div>
+    )
+  }
+}
